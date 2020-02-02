86c2246f6a8bca2a484f7df42868fec375719f83
Carlos Goering <carlosgoeringdev@outlook.com>
2020-02-02 19:47:24 -0300

    Add project files.

---
+++ CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,49 @@
+﻿
+using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+namespace CaelumEstoque.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        //
+        // GET: /Produto/
+
+        public ActionResult Index()
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            IList<Produto> produtos = dao.Lista();
+            ViewBag.Produtos = produtos;
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            CategoriasDAO categoriasDAO = new CategoriasDAO();
+            ViewBag.Categorias = categoriasDAO.Lista();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adiciona(Produto produto)
+        {
+            if(ModelState.IsValid)
+            {
+                ProdutosDAO dao = new ProdutosDAO();
+                dao.Adiciona(produto);
+                return RedirectToAction("Index", "Produto");
+            }
+            else
+            {
+                CategoriasDAO categoriasDao = new CategoriasDAO();
+                ViewBag.Categorias = categoriasDao.Lista();
+                return View("Form");
+            }
+            
+            
+            
+        }
+
+    }
+}
